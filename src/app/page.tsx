7862d5e5e8b113aae91714610c731c1d560075d4
Yagyesh <saraswatthrust@gmail.com>
2026-03-10 10:23:47 +0530

    chore: integrate TRPC with React Query and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import prisma from "@/lib/db";
+import { caller } from '@/trpc/server';
 
 const Page = async () => {
-  const users = await prisma.user.findMany();
+  const users = await caller.getUsers();
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
